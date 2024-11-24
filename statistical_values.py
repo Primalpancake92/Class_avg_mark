a0bbf6aae3ab6b46b04781259e1a45b649469840
PrimalPancake92 <johnle.jl39@gmail.com>
2024-11-24 12:05:27 +1100

    Implemented class_median() method and the std_deviation() method for the main file.

--- statistical_values.py
+++ statistical_values.py
@@ -1,15 +1,54 @@
 from classroom import Classroom
 from student import Student
-
+import math
 
 def mean_mark(classroom : Classroom) -> int:
     total = 0
     for student in classroom.class_details:
         total += student[3]
     
-    mean = total / len(classroom)
+    mean = total / len(classroom.class_details)
     
-    return f"The class mean is: {mean}"
+    return mean
+
+
+def class_median(classroom : Classroom) -> int:
+    list_of_marks = []
+    
+    for student in classroom.class_details:
+        list_of_marks.append(student[3])
+
+    # Using selection sort
+    n = len(list_of_marks)
+    for i in range(n-1):
+        min_index = i
+        for j in range(i + 1, n):
+            if list_of_marks[j] < list_of_marks[min_index]:
+                min_index = j
+
+        if min_index != i:
+            list_of_marks[i], list_of_marks[min_index] = list_of_marks[min_index], list_of_marks[i]       
+
+    mid_point = n // 2
+    
+    if n % 2 == 0:
+        median = (list_of_marks[mid_point - 1] + list_of_marks[mid_point]) / 2
+    else:
+        median = (list_of_marks[mid_point])
+    
+    return median
+
+
+def std_deviation(classroom : Classroom) -> float:
+    divisor = len(classroom.class_details)
+    dividend = 0
+
+    for student in classroom.class_details:
+        dividend += (student[3] - mean_mark(classroom))**2
+
+    std_dev = math.sqrt(dividend / divisor)
+    
+    return std_dev
 
 
 def best_student(classroom : Classroom) -> list[Student]:
